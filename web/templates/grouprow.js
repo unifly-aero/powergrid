e724ba37ffecb6ab1ff5c22a68b2a90079cb2547
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2018-07-11 11:59:12 +0200

    change req

--- web/templates/grouprow.js
+++ web/templates/grouprow.js
@@ -1,3 +1,1 @@
-define([], function() {
-    return '<span class="pg-grouping-groupindicator"><span class="pg-group-column">{{:~column.title}}</span> {{:description}} <span class="pg-group-recordcount">{{if allChildrenCount}}{{:allChildrenCount}}{{else}}{{:recordCount}}{{/if}}</span></span>'
-});
+define([], function() { return '<span class="pg-grouping-groupindicator"><span class="pg-group-column">{{:~column.title}}</span> {{:description}} <span class="pg-group-recordcount">{{:recordCount}}</span></span>'});

dd982c47eb42d54a84c331b1db56b190e14ae348
Sigurd Van Leuven <sigurdvanleuven@gmail.com>
2019-03-11 10:33:45 +0100

    AIR-5059 Translation of grouping rows

--- web/templates/grouprow.js
+++ web/templates/grouprow.js
@@ -1,1 +1,1 @@
-define([], function() { return '<span class="pg-grouping-groupindicator"><span class="pg-group-column">{{:~column.title}}</span> {{:description}} <span class="pg-group-recordcount">{{:recordCount}}</span></span>'});
+define([], function() {return '<span class="pg-grouping-groupindicator"><span class="pg-group-column">{{:~column.title}}</span> {{:~value}} <span class="pg-group-recordcount">{{:recordCount}}</span></span>'});
